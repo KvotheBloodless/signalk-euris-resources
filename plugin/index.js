d5398ec329f6d385fa80893816ef14d7afc1c052
Paul Willems <paul.willems@gmail.com>
2024-12-03 09:48:11 +0100

    Added lint config and fixes

--- plugin/index.js
+++ plugin/index.js
@@ -28,99 +28,99 @@ const loadingCache = require('@inventivetalent/loading-cache')
 const time = require('@inventivetalent/time')
 
 module.exports = function (app) {
-  const plugin = {
-    id: 'signalk-euris-resources',
-    name: 'EuRIS Resources',
-    description: 'Provides data from the EuRIS API as SignalK resources'
-  }
-
-  const lockDetailsCache = loadingCache.Caches.builder().expireAfterAccess(time.Time.minutes(60)).buildAsync(
-    lockId => {
-      app.debug(`Cache miss for lock ${lockId}`)
-      return client.lockDetails(lockId)
+    const plugin = {
+        id: 'signalk-euris-resources',
+        name: 'EuRIS Resources',
+        description: 'Provides data from the EuRIS API as SignalK resources'
     }
-  )
 
-  plugin.start = function (options) {
-    registerAsCustomResourcesProvider()
-  }
-
-  plugin.stop = function () {
-  }
-
-  plugin.schema = {
-    properties: {
-      // plugin configuration goes here
-    }
-  }
-
-  function registerAsCustomResourcesProvider () {
-    try {
-      app.registerResourceProvider({
-        type: 'EuRIS',
-        methods: {
-          listResources: (query) => {
-            app.debug(`Incoming request to list EuRIS resources - query: ${JSON.stringify(query)}`)
-
-            const locksPromise = client.listLocks(4.658717516383808, 46.96024231761558, 5.0305327978160115, 46.705876676409396).then(ids => {
-              return Promise.all(ids.map(lock => {
-                return lockDetailsCache.get(lock.id).then(details => {
-                  return lockUtils.toResourceSetFeature(lock.point, details)
-                })
-              })).then((values) => {
-                return values
-              })
-            })
-
-            return Promise.all([locksPromise]).then((values) => {
-              const resources = structuredClone(customResourcesTemplate)
-              values[0].forEach((lock) => resources[ids.Lock].values.features.push(lock))
-              return resources
-            })
-          },
-          getResource: (id, property) => {
-            app.debug(`Incoming request to get activecaptain resource - id: ${id}`)
-            throw (new Error('Not implemented!'))
-          },
-          setResource: (id, value) => {
-            throw (new Error('Not implemented!'))
-          },
-          deleteResource: (id) => {
-            throw (new Error('Not implemented!'))
-          }
+    const lockDetailsCache = loadingCache.Caches.builder().expireAfterAccess(time.Time.minutes(60)).buildAsync(
+        lockId => {
+            app.debug(`Cache miss for lock ${lockId}`)
+            return client.lockDetails(lockId)
         }
-      })
-    } catch (error) {
-      app.debug(`Cannot register as a resource provider ${error}`)
-    }
-  }
+    )
 
-  return plugin
+    plugin.start = function (options) {
+        registerAsCustomResourcesProvider()
+    }
+
+    plugin.stop = function () {
+    }
+
+    plugin.schema = {
+        properties: {
+            // plugin configuration goes here
+        }
+    }
+
+    function registerAsCustomResourcesProvider () {
+        try {
+            app.registerResourceProvider({
+                type: 'EuRIS',
+                methods: {
+                    listResources: (query) => {
+                        app.debug(`Incoming request to list EuRIS resources - query: ${JSON.stringify(query)}`)
+
+                        const locksPromise = client.listLocks(4.658717516383808, 46.96024231761558, 5.0305327978160115, 46.705876676409396).then(ids => {
+                            return Promise.all(ids.map(lock => {
+                                return lockDetailsCache.get(lock.id).then(details => {
+                                    return lockUtils.toResourceSetFeature(lock.point, details)
+                                })
+                            })).then((values) => {
+                                return values
+                            })
+                        })
+
+                        return Promise.all([locksPromise]).then((values) => {
+                            const resources = structuredClone(customResourcesTemplate)
+                            values[0].forEach((lock) => resources[ids.Lock].values.features.push(lock))
+                            return resources
+                        })
+                    },
+                    getResource: (id, property) => {
+                        app.debug(`Incoming request to get activecaptain resource - id: ${id}`)
+                        throw (new Error('Not implemented!'))
+                    },
+                    setResource: (id, value) => {
+                        throw (new Error('Not implemented!'))
+                    },
+                    deleteResource: (id) => {
+                        throw (new Error('Not implemented!'))
+                    }
+                }
+            })
+        } catch (error) {
+            app.debug(`Cannot register as a resource provider ${error}`)
+        }
+    }
+
+    return plugin
 }
 
 const lockId = 'a562d4bd-db15-4875-a262-e0b04e484e63'
 
 const ids = {
-  Lock: lockId
+    Lock: lockId
 }
 
 const customResourcesTemplate = {
-  [lockId]: {
-    type: 'ResourceSet',
-    name: 'Locks',
-    description: 'European inland waterway locks',
-    styles: {
-      default: {
-        width: 7,
-        stroke: 'black',
-        fill: 'yellow',
-        lineDash: [1, 0]
-      }
-    },
-    values: {
-      type: 'FeatureCollection',
-      features: [
-      ]
+    [lockId]: {
+        type: 'ResourceSet',
+        name: 'Locks',
+        description: 'European inland waterway locks',
+        styles: {
+            default: {
+                width: 7,
+                stroke: 'black',
+                fill: 'yellow',
+                lineDash: [1, 0]
+            }
+        },
+        values: {
+            type: 'FeatureCollection',
+            features: [
+            ]
+        }
     }
-  }
 }
